bab40ecedbfb03a1bb9f10e56634c518fc771680
Arun Umapathy <arunumap@gmail.com>
2014-02-28 11:13:11 -0500

    initial init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dailypoll::Application.config.secret_key_base = 'c115cb9123bfc7983d013e347b96c39d56764a79ce384251d2cb9776e19bbcdeb4052073a2f42a662eb63db9e092dcfac1d49c8772e7b5fdcabbff8d5b9e58f4'
