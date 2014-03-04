97683bca04b88e17f1993b4839b273aee02ee2f0
Arun Umapathy <arunumap@gmail.com>
2014-03-04 11:56:26 -0500

    working code finalized backend refactoring

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,27 +3,32 @@ class Vote < ActiveRecord::Base
 	belongs_to :question
 
 	def save_the_vote(user, text)
-		#Should extract this to helper method - duplicate cod
 		question = Question.active
 		
 		self.user_id = user.id
 		self.question_id = question.id
+		text_response = process_text(text)
 		
-		vote = Vote.find_by(:user_id => user.id, :question_id => question.id)
-		if vote.nil?
-			if text == 'Yes'
-				self.vote = 1
-			else 
-				self.vote = 0
-			end
+		existing_vote = Vote.find_by(:user_id => user.id, :question_id => question.id)
+		# if vote is nil, then we want to create a new instance of vote and persist
+		if existing_vote.nil?
+			text_response == 'y' ? self.vote = 1 : self.vote = 0
 			self.save
-		else 
-			if text == "Yes"
-				vote.vote = 1
-			else
-				vote.vote = 0
-			end
-			vote.save
+		else
+		#if vote is not nil, then we want to update the current instance of vote and persist 
+			text_response == "y" ? existing_vote.vote = 1 : existing_vote.vote = 0
+			existing_vote.save
 		end
 	end
+
+	def process_text(text)
+		text.downcase!
+		if /[yes]/ === text
+			return "y"
+		else
+			return "n"
+		end
+	end
+
+
 end
