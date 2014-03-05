ba941ec8867df41d34a9d859752bf769395da8d3
Arun Umapathy <arunumap@gmail.com>
2014-03-05 15:09:52 -0500

    formated nav bar icons and text w/ spacing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Dailypoll::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  
 end
