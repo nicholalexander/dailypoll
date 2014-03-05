d340a9e98f10d3855ddb1eceb6c34e4d26c32518
Arun Umapathy <arunumap@gmail.com>
2014-03-05 10:14:40 -0500

    initial commit for march 5, updated js files

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -5,18 +5,39 @@ window.onload = function() {
 	console.log($("#myChart"))
 	console.log(gon.yes_votes)
 	console.log(gon.no_votes)
-	data = [
-	{
-		value: gon.no_votes,
-		color: "#F7464A"
-	},
-	{
-		value: gon.yes_votes,
-		color: "#E2EAE9"
-	}];
+	// data = [
+	// {
+	// 	value: gon.no_votes,
+	// 	color: "#F7464A"
+	// },
+	// {
+	// 	value: gon.yes_votes,
+	// 	color: "#E2EAE9"
+	// }];
 
 
 
-	var ctx = $("#myChart").get(0).getContext("2d");
-	myNewChart = new Chart(ctx).Doughnut(data);
+	// var ctx = $("#myChart").get(0).getContext("2d");
+	// myNewChart = new Chart(ctx).Doughnut(data);
+
+
+	var hash_keys = Object.keys(gon.question_hash)
+
+	var hash_length = hash_keys.length
+
+	for (var i = 0; i < hash_length; i++) {
+		target_chart = hash_keys[i]
+		ctx = $("#" + target_chart).get(0).getContext("2d");
+		data = [
+					{
+						value: gon.question_hash[hash_keys[i]].yes,
+						color: "#F7464A"
+					},
+					{
+						value: gon.question_hash[hash_keys[i]].no,
+						color: "#E2EAE9"
+					}];
+		index_charts = new Chart(ctx).Doughnut(data);
+	}
+
 }
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,27 @@ class QuestionsController < ApplicationController
 	respond_to :html, :js, :json
 
 	def index
+		@questions = Question.all
+		yes_votes = 0
+		no_votes = 0
+		@question_hash = {}
+		@questions.each do |question|
+			question.votes.each do |vote|
+				if vote.vote == 1
+					yes_votes += 1
+				else
+					no_votes += 1
+				end
+			end
+			@question_hash[question.id] = {yes: yes_votes, no: no_votes}
+			yes_votes = 0
+			no_votes = 0
+		end
 
+		#send question hash to view and to javascript
+		gon.question_hash = @question_hash
+
+		#send all questions and question hash to view
 	end
 
 	def show
